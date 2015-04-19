3fecb6d9f17bced778c40c04f8b53bf9523dfc9d
Mikhail Kashkin <m@xen.ru>
2015-04-19 03:19:19 +0300

    who use 2 spaces for code?

--- tests/lang_test.py
+++ tests/lang_test.py
@@ -5,8 +5,8 @@ from wikipedia import wikipedia
 
 
 class TestLang(unittest.TestCase):
-  """Test the ability for wikipedia to change the language of the API being accessed."""
+    """Test the ability for wikipedia to change the language of the API being accessed."""
 
-  def test_lang(self):
-    wikipedia.set_lang("fr")
-    self.assertEqual(wikipedia.API_URL, 'http://fr.wikipedia.org/w/api.php')
+    def test_lang(self):
+        wikipedia.set_lang("fr")
+        self.assertEqual(wikipedia.API_URL, 'http://fr.wikipedia.org/w/api.php')
